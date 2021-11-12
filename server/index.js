9ffeec6f53c5f724e1b6fb461ff23236dab6ba6c
Prakhar Sharma <76238592+sprakhar11@users.noreply.github.com>
2021-11-12 22:55:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,19 +3,12 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server( { port: 8082 });
 
 wss.on("connection" , ws => {
-    //console.log("new client connected");
-
     ws.on('message', function(message) {
-        console.log("Received: "+message);
-        
-        wss.clients.forEach(function e(client) {
-            if (client != ws)
-                client.send(""+message);
+        wss.clients.forEach(function (client) {
+            if (client !== ws){
+                client.send(message);
+            }
         });
-
-
-
-        //ws.send("From server : "+message);
     });
 
     ws.on("close", () => {
